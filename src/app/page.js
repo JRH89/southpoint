584048acfc46484e8530edc26756a4a9680f7637
JRH89 <thegemlins@gmail.com>
2024-03-03 18:35:44 -0800

    ok

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 'use client'
 
+// components/GalleryComponent.js
+
 import Image from 'next/image'
 import React, { useState } from 'react'
 
@@ -7,55 +9,32 @@ const GalleryComponent = () => {
   const houses = [
     {
       name: '1A',
-      images: [
-        '1A/Front.png',
-
-      ],
+      images: ['1A/Front.png'],
     },
     {
       name: '1B',
-      images: [
-        '1B/Front.png',
-
-      ],
+      images: ['1B/Front.png'],
     },
     {
       name: '2A',
-      images: [
-        '2A/Front.png',
-
-      ],
+      images: ['2A/Front.png'],
     },
     {
       name: '2B',
-      images: [
-        '2B/Front.png',
-
-      ],
+      images: ['2B/Front.png'],
     },
     {
       name: '3A',
-      images: [
-        '3A/Front.png',
-
-      ],
+      images: ['3A/Front.png'],
     },
     {
       name: '3B',
-      images: [
-        '3B/Front.png',
-
-      ],
+      images: ['3B/Front.png'],
     },
     {
       name: 'Casitas',
-      images: [
-        'Casitas/Casita-A.png',
-        'Casitas/Casita-B.png',
-
-      ],
+      images: ['Casitas/Casita-A.png', 'Casitas/Casita-B.png'],
     },
-
   ]
 
   const [selectedHouse, setSelectedHouse] = useState(null)
@@ -75,19 +54,27 @@ const GalleryComponent = () => {
 
     setCurrentImageIndex(newIndex)
   }
+
   return (
     <div>
-
       <div className="flex bg-gray-100 h-screen text-gray-500">
-
         <div className="w-1/4 p-4 bg-gray-300">
-          <h1 className='w-full text-center mx-auto py-2 my-2 text-gray-500 text-xl bg-gray-100 self-center items-center content-center'>South Point 13</h1>
+          <h1
+            onClick={() => {
+              setSelectedHouse(null)
+              setCurrentImageIndex(0)
+            }}
+            className="cursor-pointer w-full text-center mx-auto py-2 my-2 text-gray-500 text-xl bg-gray-100 self-center items-center content-center"
+          >
+            South Point 13
+          </h1>
+
           <ul>
             {houses.map((house, index) => (
               <li
                 key={index}
                 onClick={() => handleHouseClick(index)}
-                className={`cursor-pointer p-2 mb-2 ${selectedHouse === index ? 'bg-white' : 'bg-gray-200'
+                className={`cursor-pointer text-center hover:ml-4 duration-200 p-2 mb-2 ${selectedHouse === index ? 'bg-white' : 'bg-gray-200'
                   }`}
               >
                 {house.name}
@@ -96,43 +83,62 @@ const GalleryComponent = () => {
           </ul>
         </div>
         <div className="w-3/4 px-2 py-8">
-          {selectedHouse !== null && (
-            <div>
-              <Image
-                width={1920}
-                height={1080}
-                quality={100}
-                src={`/images/Renders/${houses[selectedHouse].images[currentImageIndex]}`}
-                alt={`${houses[selectedHouse].name} Image`}
-                className="rounded-lg shadow-lg"
-              />
+          <div>
+            {selectedHouse !== null && (
+              <>
+                <Image
+                  width={1920}
+                  height={1080}
+                  quality={100}
+                  src={`/images/Renders/${houses[selectedHouse].images[currentImageIndex]}`}
+                  alt={`${houses[selectedHouse].name} Image`}
+                  className="shadow-lg"
+                />
 
-              <div className="mt-4 flex justify-between">
-                <button
-                  onClick={() => handleArrowClick('left')}
-                  className="bg-gray-200 px-4 py-2 rounded-lg"
-                >
-                  Left
-                </button>
-                <button
-                  onClick={() => handleArrowClick('right')}
-                  className="bg-gray-200 px-4 py-2 rounded-lg"
-                >
-                  Right
-                </button>
-              </div>
+                <div className="mt-4 flex justify-between">
+                  <button
+                    onClick={() => handleArrowClick('left')}
+                    className="bg-gray-200 px-4 py-2"
+                  >
+                    Left
+                  </button>
+                  <button
+                    onClick={() => handleArrowClick('right')}
+                    className="bg-gray-200 px-4 py-2"
+                  >
+                    Right
+                  </button>
+                </div>
+              </>
+            )}
+            {selectedHouse == null && (
+              <>
+                <Image
+                  width={1500}
+                  height={800}
+                  alt='cover'
+                  src={'/images/SouthPointCover.png'}
+                />
+                <p>
+                  Southpoint sits at the top of South Main Street, designed to
+                  take advantage of the breathtaking views of the valley.
+                  Homebuyers will enjoy the large lots suitable for single and
+                  two-story houses.
+                </p>
+              </>
 
-            </div>
-          )}
-
-
+            )}
+          </div>
         </div>
       </div>
-      <footer className='bg-gray-100 text-gray-500 text-center py-1'>
-        <p><a className='hover:underline' href='www.hookerhillstudios.com'>Hooker Hill Studios</a></p>
+      <footer className="bg-gray-100 text-gray-500 text-center py-1">
+        <p>
+          <a className="hover:underline" href="www.hookerhillstudios.com">
+            Hooker Hill Studios
+          </a>
+        </p>
       </footer>
     </div>
-
   )
 }
 
