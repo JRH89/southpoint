2bd75f9bfa3b1722e2c78273ef9dcd35ff0f7fdb
JRH89 <thegemlins@gmail.com>
2024-03-03 19:18:42 -0800

    ok

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "South Point 13",
-  description: "Located in Corona, California",
+  description: "Southpoint sits at the top of South Main Street, designed to take advantage of the breathtaking views of the valley. Homebuyers will enjoy the large lots suitable for single and two-story houses."
 }
 
 export default function RootLayout({ children }) {
